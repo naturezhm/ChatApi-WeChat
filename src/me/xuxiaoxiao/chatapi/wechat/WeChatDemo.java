43cd06d8598cf13cd72b81a2481c37832c9c0597
XXX <xxx@xuxiaoxiao.me>
2017-05-20 13:45:54 +0800

    增加了Demo代码，修改了README文件

--- src/me/xuxiaoxiao/chatapi/wechat/WeChatDemo.java
+++ src/me/xuxiaoxiao/chatapi/wechat/WeChatDemo.java
@@ -1,21 +1,11 @@
-# ChatApi-WeChat
-微信聊天接口，可用于制作微信聊天机器人
+package me.xuxiaoxiao.chatapi.wechat;
 
-## 这是什么？
-* 这是一个简易的微信聊天客户端
-* 该客户端使用的接口来自于网页版微信
+import me.xuxiaoxiao.chatapi.wechat.entity.AddMsg;
 
-## 有何优点？
-* 对接口和流程进行了封装，更加简单易用
-* 暴露了一个监听器，可以自己实现监听器以开发自己的业务功能
+import java.net.CookieHandler;
+import java.net.CookieManager;
+import java.util.logging.Level;
 
-## 待改进的地方
-* 目前只能解析和发送文字类的消息
-
-## 如何使用
-* 首先下载必要的依赖，[gson](https://github.com/google/gson)和[xtools-common](https://github.com/xuxiaoxiao-xxx/XTools-Common)
-* 以下是一个学别人说话的小机器人，用到了该库提供的大部分功能
-```java
 public class WeChatDemo {
     //网页微信登录时有两个重要的值（wxsid，wxuin）是在cookie中返回的，这里使用了默认的内存Cookie管理器
     public static final CookieManager cookieManager = new CookieManager();
@@ -75,4 +65,3 @@ public class WeChatDemo {
         //wechatClient.shutdown();
     }
 }
-```
